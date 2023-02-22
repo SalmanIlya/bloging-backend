aae7f2366f52ce28fa3f344c16b1713faf493edf
Salamanilyas <salamanilyas101@gmail.com>
2023-02-22 14:42:33 +0500

    user-login-and-regsister

---
+++ Router/auth.js
@@ -0,0 +1,51 @@
+const express=require("express");
+const User = require("../module/User");
+const cryptojs=require("crypto-js")
+const jwt=require("jsonwebtoken")
+// const authorization=require("../middlewear/middlewear");
+const dotenv=require("dotenv")
+dotenv.config({path:"config/config.env"})
+const app=express.Router()
+
+app.post("/regsister",async(req,res)=>{
+try{
+const user =await User.create({
+    name:req.body.name,
+    email:req.body.email,
+    password:cryptojs.RC4.encrypt(
+        req.body.password,process.env.secritkey
+    ).toString()
+})
+res.send(user)
+}catch(err){
+    console.log(err);
+}
+})
+
+
+app.post("/login",async(req,res)=>{
+    try{
+        const user=await User.findOne({name:req.body.name})
+        const {password,...other}=user._doc
+        if(user){
+            const pass=cryptojs.RC4.decrypt(password,process.env.secritkey).toString(cryptojs.enc.Utf8)
+            if(pass===req.body.password){
+                const token=jwt.sign(
+                    {
+                        id:user.id,
+                        isAdmin:user.isAdmin
+                    }, process.env.tocken
+                )
+                res.send({...other,token})
+
+            }else{
+                res.send("incorrest password")
+            }
+        }else{
+            res.send("user not found")
+        }
+}catch(err){
+        res.send("server error :",err)
+    }})
+ 
+module.exports=app
